0eea232072fe21198e1b2708cfac0df13b1e9c33
RayendraSabandar <rayendratimotius@gmail.com>
2021-11-19 09:05:33 +0700

    Revised dateFormat helper

--- src/helpers/dateFormat.js
+++ src/helpers/dateFormat.js
@@ -1,12 +1,17 @@
 function fullMonthFormat(date){
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
     const newDate = date.split('/')
     const year = newDate[2]
-    const numberMonth = newDate[1]
     const day = newDate[0]
-
-    const month = months[numberMonth - 1];
+    const numberMonth = newDate[1]
+    
+    let month = ''
+    if(+numberMonth){
+        month = months[+numberMonth - 1]
+    } else {
+        month = numberMonth
+    }
+    
     const fullDate = `${day} ${month} ${year}`
     return fullDate
 }
