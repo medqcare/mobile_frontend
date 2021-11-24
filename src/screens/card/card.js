d906ef5ae27673711ae950ce21a6f38d4bfe972f
halim syarif <halimsyarif@192.168.1.22>
2021-11-24 16:14:50 +0700

    fixed card-page if no card available

--- src/screens/card/card.js
+++ src/screens/card/card.js
@@ -123,8 +123,8 @@ const card = props => {
     {
       hospital &&
         hospital.map(item => {
-          if (item.facilityID.facilityMainType == facility) {
-            setAktif(item.facilityID.facilityMainType)
+          if (item.facilityID?.facilityMainType == facility) {
+            setAktif(item.facilityID?.facilityMainType)
             temp.push(item)
           }
         })
