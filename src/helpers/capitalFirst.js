fe4223f70eceb1fc105cae49eaaf73a690d62519
Rayendra Sabandar <rayendratimotius@gmail.com>
2021-11-29 11:19:20 +0700

    Added validation; DKI and DI will remain capitalized

--- src/helpers/capitalFirst.js
+++ src/helpers/capitalFirst.js
@@ -5,13 +5,17 @@ export default function capitalFirst (str) {
     
     for(let i = 0; i < array.length; i++){
         const word = array[i]
-        if(i === 0){
+        if(word === 'DKI' || word === 'DI'){
+            result += word
+        }
+        else if(i === 0){
             firstWord += word[0].toUpperCase() + word.slice(1).toLowerCase()
             result += firstWord
         } else {
             result += ' ' + word[0].toUpperCase() + word.slice(1).toLowerCase()
         }
     }
+
    
     return result
 }
