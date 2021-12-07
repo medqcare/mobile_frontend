4b2df6df8b8850ab6493a3179547febd4d51533a
halim syarif <halimsyarif@192.168.1.24>
2021-12-07 10:10:35 +0700

    create rujukan page

---
+++ src/navigation/home/rujukanStack.js
@@ -0,0 +1,16 @@
+import { createStackNavigator } from "react-navigation-stack";
+import RujukanList from '../../screens/home/rujukan/RujukanList'
+
+export default StackRujukan = createStackNavigator(
+  {
+    ListRujukan: {
+      screen: RujukanList,
+      navigationOptions: {
+        headerShown: false
+      },
+    }
+  },
+  {
+    initialRouteName: "ListRujukan"
+  }
+);
