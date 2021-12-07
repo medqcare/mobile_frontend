ffd83f24b1d0188803e49c0302db9987396a7667
Rayendra Sabandar <rayendratimotius@gmail.com>
2021-12-07 15:36:30 +0700

    File name in digitalocean is the full name of the user

--- src/helpers/formData.js
+++ src/helpers/formData.js
@@ -1,17 +1,18 @@
-
-
-export default function createFormData(file){
-    console.log('Application is creating form data')
+export default function createFormData(file, fullName){
     
-    const encode = file.base64
+    // Location of the image on the device
     let localUri = file.uri;
     let filename = localUri.split('/').pop();
+    
+    // The name of the uploaded image (you can search in https://cloud.digitalocean.com/spaces/pic-cis.applimetis.id?i=ab2c5e)
+    fullName = fullName.replace(/\s+/g, '').toLowerCase()
     let match = /\.(\w+)$/.exec(filename);
     let type = match ? `image/${match[1]}` : `image`
-    const data = new FormData();
-    data.append('avatar', { uri: localUri, name: filename, type })
-    // data.append('avatar', encode);
     
+    
+    console.log('Application is creating form data')
+    const data = new FormData();
+    data.append('avatar', { uri: localUri, name: fullName, type })
     console.log('Form data created')
 
     return data
