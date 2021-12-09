2c31ab3e6f853330176f845af6b157dbf72b93cc
Rayendra Sabandar <rayendratimotius@gmail.com>
2021-12-09 18:14:56 +0700

    Created reminder page

--- src/screens/home/reminder/Reminder.js
+++ src/screens/home/reminder/Reminder.js
@@ -4,44 +4,126 @@ import {
   Text,
   View,
   Dimensions,
+  Image,
+  TouchableOpacity
 } from "react-native";
-import Header from "../../../components/headers/GradientHeader";
+import { connect } from "react-redux";
+import Header from "../../../components/headers/ReminderHeader";
+import ReminderActiveList from "./ReminderActiveList";
+import ReminderFinishedList from "./ReminderFinishedList";
 
 const dimHeight = Dimensions.get("window").height;
 const dimWidth = Dimensions.get("window").width;
 
-export default function Reminder(props) {
+function Reminder(props) {
 
-  const data = null
+	const userData = props.userData
+	function firstName(){
+		return userData.firstName.split(' ')[0]
+	}
+
+	const [selectedStatus, setSelectedStatus] = useState('Active')
+
+	async function changeStatus(status){
+		setSelectedStatus(status)
+	}
   
-  return (
-    <View style={styles.container}>
-      <Header title="Reminder" navigate={props.navigation.navigate} />
-      <View style={styles.content}>
-        {
-          data ? (  
-            <Text style={styles.textItem}>Ini datanya </Text>
-          ) : (
-            <Text style={styles.textItem}>Belum Ada Reminder</Text>
-          )
-        }
-      </View>
-    </View>
-  );
+  	return (
+		<View style={styles.container}>
+			<Header
+				navigate={props.navigation.navigate}
+				name={firstName()}
+			/>
+			<View style={styles.content}>
+				<View style={styles.options}>
+					<View style={styles.statusContainer}>
+						<TouchableOpacity
+							activeOpacity={1}
+							style={selectedStatus === 'Active' ? styles.selectedStatusInnerContainer: styles.unSelectedStatusInnerContainer}
+							onPress={() => changeStatus('Active')}
+						>
+							<Text style={selectedStatus === 'Active' ? styles.selectedStatusText: styles.unSelectedStatusText}>Aktif</Text>
+						</TouchableOpacity>
+						<TouchableOpacity
+							activeOpacity={1}
+							onPress={() => changeStatus('Finished')}
+							style={selectedStatus === 'Finished' ? styles.selectedStatusInnerContainer: styles.unSelectedStatusInnerContainer}
+						>
+							<Text style={selectedStatus === 'Finished' ? styles.selectedStatusText: styles.unSelectedStatusText}>Selesai</Text>
+						</TouchableOpacity>
+					</View>
+					<View style={styles.optionAdd}>
+						<Image
+							source={require('../../../assets/png/ReminderAddButton.png')}
+						/>
+					</View>
+				</View>
+				{selectedStatus === 'Active' ? 
+					<ReminderActiveList/> :
+					<ReminderFinishedList/>
+				}
+			</View>
+		</View>
+  	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#181818",
-  },
-  content: {
-    height: dimHeight * 0.88,
-    justifyContent: "space-between",
-    alignItems: 'center',
-    padding: 20,
-  },
-  textItem: {
-    color: "#B5B5B5",
-  }
+	container: {
+		flex: 1,
+		backgroundColor: "#1F1F1F",
+	},
+	
+	content: {
+		height: dimHeight * 0.88,
+		justifyContent: "flex-start",
+		alignItems: 'center',
+	},
+
+	options: {
+		width: dimWidth,
+		flexDirection: 'row',
+		justifyContent: "space-between",
+	},
+	
+	statusContainer: {
+		flexDirection: "row",
+		justifyContent: 'space-evenly',
+		width: dimWidth * 0.55,
+	},
+
+	selectedStatusInnerContainer: {
+		borderBottomWidth: 1,
+		borderBottomColor: "rgba(119, 191, 244, 1)",
+		paddingHorizontal: dimWidth * 0.024,
+		paddingVertical: dimHeight * 0.0196
+	},
+
+	unSelectedStatusInnerContainer: {
+		paddingHorizontal: dimWidth * 0.024,
+		paddingVertical: dimHeight * 0.0196
+	},
+
+	selectedStatusText: {
+		color: "rgba(119, 191, 244, 1)",
+		fontSize: 12,
+	},
+
+	unSelectedStatusText: {
+		color: "#B5B5B5",
+		fontSize: 12,
+	},
+
+	optionAdd: {
+		paddingTop: dimHeight * 0.0196,
+		paddingRight: dimWidth * 0.04167
+	},
 });
+
+
+
+
+const mapStateToProps = state => {
+    return state
+}
+
+export default connect(mapStateToProps)(Reminder)
