d5ef8733fed558e79c42ee879eef3c59477bf6b6
Rayendra Sabandar <rayendratimotius@gmail.com>
2021-12-17 17:39:29 +0700

    Registered DrugDetail screen in reminder stack

--- src/navigation/home/reminderStack.js
+++ src/navigation/home/reminderStack.js
@@ -1,6 +1,7 @@
 import { createStackNavigator } from "react-navigation-stack";
 import Reminder from "../../screens/home/reminder/Reminder";
 import AddReminderForm from "../../screens/home/reminder/AddReminderForm";
+import DrugDetail from "../../screens/home/reminder/DrugDetail";
 
 export default StackReminder = createStackNavigator(
   {
@@ -15,7 +16,13 @@ export default StackReminder = createStackNavigator(
       navigationOptions: {
         headerShown: false
       }
-    }
+    },
+    DrugDetail: {
+      screen: DrugDetail,
+      navigationOptions: {
+        headerShown: false
+      }
+    },
   },
   {
     initialRouteName: "Reminder"
