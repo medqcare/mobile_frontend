fac9d244e1182c314acdc9afb8f0e75681cd45dc
Rayendra Sabandar <rayendratimotius@gmail.com>
2021-12-20 09:58:03 +0700

    Changed VerticalLine height

--- src/assets/svg/VerticalLine.js
+++ src/assets/svg/VerticalLine.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Svg, { Path } from 'react-native-svg'
 
 const SvgComponent = props => (
-  <Svg width="2" height="21" viewBox="0 0 2 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <Svg width="2" height="23" viewBox="0 0 2 21" fill="none" xmlns="http://www.w3.org/2000/svg">
     <Path d="M0.970703 20.1758L0.970703 0.587547" stroke="#B5B5B5" stroke-width="1.05882"/>
   </Svg>
 )
