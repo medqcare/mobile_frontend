76ec9c008e7d1973aa08eb05a9ce58acf03d1a16
Rayendra Sabandar <rayendratimotius@gmail.com>
2021-12-21 17:31:40 +0700

    Created reducer for prescriptions and reminders

--- src/stores/reducers/index.js
+++ src/stores/reducers/index.js
@@ -58,6 +58,22 @@ export default (state = initState, action) => {
         ...state,
         userData: action.payload
       }
+    case 'GET_PRESCRIPTION_DATA':
+      return {
+        ...state,
+        userData: {
+          ...state.userData,
+          prescriptions: action.payload
+        }
+      }
+    case 'GET_REMINDER_DATA':
+      return {
+        ...state,
+        userData: {
+          ...state.userData,
+          reminderDetails: action.payload
+        }
+      }
     case 'SET_TODAY_ACTIVITY':
       return {
         ...state,
