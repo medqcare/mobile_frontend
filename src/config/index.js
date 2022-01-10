3f4ce99b047b5733a8f266edfc2ef8281f6eacb0
enjaytarigan <tariganagc@gmail.com>
2022-01-10 09:36:18 +0700

    change image profile

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,8 @@
-let baseURL = 'https://api-medqcare.applimetis.id';
-let webBaseURL = 'https://f913-182-2-165-116.ngrok.io' // URL for postgres drug database
+// let baseURL = 'https://api-medqcare.applimetis.id';
+let baseURL = 'https://2843-180-252-126-41.ngrok.io';
+let webBaseURL = 'https://f913-182-2-165-116.ngrok.io'; // URL for postgres drug database
 
 module.exports = {
   baseURL,
-  webBaseURL
+  webBaseURL,
 };
