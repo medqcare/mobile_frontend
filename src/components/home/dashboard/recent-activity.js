51f74bc87841e362ad9621199f35d93d0b4ab68c
enjaytarigan <tariganagc@gmail.com>
2022-01-13 17:17:25 +0700

    optimzation recent activity navigation

--- src/components/home/dashboard/recent-activity.js
+++ src/components/home/dashboard/recent-activity.js
@@ -8,6 +8,7 @@ import {
   ScrollView,
   Dimensions,
 } from 'react-native';
+import { widthPercentageToDP } from 'react-native-responsive-screen';
 
 const heightDim = Dimensions.get('screen').height;
 const widthDim = Dimensions.get('screen').width;
@@ -19,81 +20,69 @@ const RecentActivity = ({ navigation }) => {
       showsHorizontalScrollIndicator={false}
       style={{ flexDirection: 'row', justifyContent: 'space-between' }}
     >
-      <View style={[style.container]}>
-        <View style={style.borderIcon}>
-          <TouchableOpacity
-            style={style.clickArea}
-            onPress={() => navigation.navigate('Appointment')}
-          >
-            <Image
-              source={require('../../../assets/png/ic_aplist.png')}
-              style={{ width: 25, height: 25 }}
-            />
-            <Text
-              style={{
-                fontSize: 12,
-                color: '#DDDDDD',
-                marginTop: 8,
-                textAlign: 'center',
-              }}
-            >
-              Appointment
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      <TouchableOpacity
+        style={style.container}
+        onPress={() => navigation.navigate('Appointment')}
+      >
+        <Image
+          source={require('../../../assets/png/ic_aplist.png')}
+          style={{ width: 25, height: 25 }}
+        />
+        <Text
+          style={{
+            fontSize: 12,
+            color: '#DDDDDD',
+            marginTop: 8,
+            textAlign: 'center',
+          }}
+        >
+          Appointment
+        </Text>
+      </TouchableOpacity>
 
-      <View style={[style.container]}>
-        <View style={style.borderIcon}>
-          <TouchableOpacity
-            style={style.clickArea}
-            onPress={() => {
-              navigation.navigate('ActivityStack');
-            }}
-          >
-            <Image
-              source={require('../../../assets/png/ic_antrian.png')}
-              style={{ width: 25, height: 25 }}
-            />
-            <Text
-              style={{
-                fontSize: 12,
-                color: '#DDDDDD',
-                marginTop: 8,
-                textAlign: 'center',
-              }}
-            >
-              Antrian
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      <TouchableOpacity
+        style={style.container}
+        onPress={() => {
+          navigation.navigate('ActivityStack');
+        }}
+      >
+        <Image
+          source={require('../../../assets/png/ic_antrian.png')}
+          style={{ width: 25, height: 25 }}
+        />
+        <Text
+          style={{
+            fontSize: 12,
+            color: '#DDDDDD',
+            marginTop: 8,
+            textAlign: 'center',
+          }}
+        >
+          Antrian
+        </Text>
+      </TouchableOpacity>
 
-      <View style={[style.container]}>
-        <View style={style.borderIcon}>
-          <TouchableOpacity
-            style={style.clickArea}
-            onPress={() => {
-              navigation.navigate('MedicalStats', { goback: 'Home' });
-            }}
-          >
-            <Image
-              source={require('../../../assets/png/Medical.png')}
-              style={{ width: 26, height: 26 }}
-            />
-            <Text
-              style={{
-                fontSize: 12,
-                color: '#DDDDDD',
-                marginTop: 8,
-                textAlign: 'center',
-              }}
-            >
-              Resume Medis
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      <TouchableOpacity
+        style={style.container}
+        onPress={() => {
+          navigation.navigate('MedicalStats', { goback: 'Home' });
+        }}
+      >
+        <Image
+          source={require('../../../assets/png/Medical.png')}
+          style={{ width: 26, height: 26 }}
+        />
+        <Text
+          style={{
+            fontSize: 12,
+            color: '#DDDDDD',
+            marginTop: 8,
+            textAlign: 'center',
+          }}
+        >
+          Resume Medis
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -108,11 +97,17 @@ const style = StyleSheet.create({
     shadowOpacity: 0.21,
     shadowRadius: 9.11,
     elevation: 10,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 3,
+    paddingVertical: 15,
+    backgroundColor: '#2F2F2F',
+    width: widthPercentageToDP('28%'),
+    height: heightDim * 0.1,
   },
 
   borderIcon: {
-    height: heightDim * 0.1,
-    width: widthDim * 0.28,
     backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
