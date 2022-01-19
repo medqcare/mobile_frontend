26395fdc5d45e563d6dc74a22c9463c9a5e38301
Rayendra Sabandar <rayendratimotius@gmail.com>
2022-01-19 10:13:17 +0700

    Added descriptive console.log message when trying to find allergy

--- src/stores/action/index.js
+++ src/stores/action/index.js
@@ -1016,12 +1016,16 @@ export function getAlergie(patientId, token) {
   return (dispatch) => {
     return new Promise(async (resolve, reject) => {
       try {
+        console.log(`Application is trying to find patient's allergies`)
         let data = await instance({
           url: `/v1/members/alergies/${patientId}`,
           method: 'GET',
           headers: { Authorization: token },
         });
         resolve(data.data);
+        if(data.data.data.length > 1) console.log(`Application found ${data.data.data.length} allergies for the selected patient`)
+        else console.log(`Application found ${data.data.data.length} allergy for the selected patient`)
+        
       } catch (err) {
         console.log(err.response.status, 'ini kembalian data get alergie');
         reject(err.response.status);
