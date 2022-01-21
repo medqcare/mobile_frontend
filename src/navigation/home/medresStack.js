006e7c3ded2006a98efa0395f84d46e175ae02da
enjaytarigan <tariganagc@gmail.com>
2022-01-21 09:12:39 +0700

    share medres implemented

--- src/navigation/home/medresStack.js
+++ src/navigation/home/medresStack.js
@@ -4,6 +4,7 @@ import {
 } from 'react-navigation-stack';
 import medicalStats from '../../screens/home/medicalStats/MedicalStats';
 import DetailResumeMedis from '../../screens/home/medicalStats/DetailResumeMedis';
+import ScanMedres from '../../screens/home/medicalStats/ScanMedres';
 
 // import MedresList from '../../screens/home/medicalStats/MedicalStats'
 // import DetailMedres from '../../components/home/medicalStats/detailMedicalStats'
@@ -21,6 +22,12 @@ export default StackMedres = createStackNavigator(
     },
     DetailResumeMedis: {
       screen: DetailResumeMedis,
+      navigationOptions: {
+        headerShown: false,
+      },
+    },
+    ScannerShareMedres: {
+      screen: ScanMedres,
       navigationOptions: {
         headerShown: false,
       },
