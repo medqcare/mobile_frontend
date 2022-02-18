73bf51b39cc36a0b9c0f732d1c72b85b63c9616f
enjaytarigan <tariganagc@gmail.com>
2022-02-18 09:53:09 +0700

    fixing flashing in profile screen

--- src/navigation/profile/index.js
+++ src/navigation/profile/index.js
@@ -1,103 +1,112 @@
-import { createStackNavigator, TransitionPresets } from 'react-navigation-stack'
-import settingStack from '../../navigation/profile/settingStack'
-import ProfilePage from '../../screens/profile/profile'
-import AddFamilyPage from './addFamilyStack'
-import InsurancePage from './paymentStack'
-import MedicalHistoryPage from './medicalHistoryStack'
-import HistoryPage from '../../components/profile/dashboard/historyRegistration'
-import ResetPasswordPage from  '../../navigation/profile/resetPasswordStack'
-import ProfileDetailStack from './profileStack'
-import ProfilePictureGallery from '../../screens/profile/profilePictureGallery'
-import profilePictureCamera from '../../screens/profile/profilePictureCamera'
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from 'react-navigation-stack';
+import settingStack from '../../navigation/profile/settingStack';
+import ProfilePage from '../../screens/profile/profile';
+import AddFamilyPage from './addFamilyStack';
+import InsurancePage from './paymentStack';
+import MedicalHistoryPage from './medicalHistoryStack';
+import HistoryPage from '../../components/profile/dashboard/historyRegistration';
+import ResetPasswordPage from '../../navigation/profile/resetPasswordStack';
+import ProfileDetailStack from './profileStack';
+import ProfilePictureGallery from '../../screens/profile/profilePictureGallery';
+import profilePictureCamera from '../../screens/profile/profilePictureCamera';
 
-
-export default ProfileTab = createStackNavigator({
+export default ProfileTab = createStackNavigator(
+  {
     ProfileStack: {
-        screen: ProfilePage,
-        navigationOptions: {
-            headerShown: false,
-        }
+      screen: ProfilePage,
+      navigationOptions: {
+        headerShown: false,
+      },
     },
     ProfilePictureGallery: {
-        screen: ProfilePictureGallery,
-        navigationOptions: {
-            headerShown: false
-        }
+      screen: ProfilePictureGallery,
+      navigationOptions: {
+        headerShown: false,
+      },
     },
     ProfilePictureCamera: {
-        screen: profilePictureCamera,
-        navigationOptions: {
-            headerShown: false
-        }
+      screen: profilePictureCamera,
+      navigationOptions: {
+        headerShown: false,
+      },
     },
     ProfileDetail: {
-        screen: ProfileDetailStack,
-        navigationOptions: {
-            headerShown: false,
-        },
+      screen: ProfileDetailStack,
+      navigationOptions: {
+        headerShown: false,
+      },
     },
     SettingStack: {
-        screen: settingStack,
-        navigationOptions: {
-            headerShown: false,
-        }
+      screen: settingStack,
+      navigationOptions: {
+        headerShown: false,
+      },
     },
     AddFamily: {
-        screen: AddFamilyPage,
-        navigationOptions: {
-            headerShown: false,
-            tabBarVisible: () => {
-                let tabBarVisible = true
-                if (navigation.state.index > 0) {
-                    tabBarVisible = false
-                }
-                return {
-                    tabBarVisible,
-                }
-            },
-        }
+      screen: AddFamilyPage,
+      navigationOptions: {
+        headerShown: false,
+        tabBarVisible: () => {
+          let tabBarVisible = true;
+          if (navigation.state.index > 0) {
+            tabBarVisible = false;
+          }
+          return {
+            tabBarVisible,
+          };
+        },
+      },
     },
     ResetPassword: {
-        screen: ResetPasswordPage,
-        navigationOptions: {
-            headerShown: false,
-            tabBarVisible: () =>  {
-                let tabBarVisible = true
-                if (navigation.state.index > 0){
-                    tabBarVisible = false
-                }
-                return {
-                    tabBarVisible
-                }
-            },
-        }
+      screen: ResetPasswordPage,
+      navigationOptions: {
+        headerShown: false,
+        tabBarVisible: () => {
+          let tabBarVisible = true;
+          if (navigation.state.index > 0) {
+            tabBarVisible = false;
+          }
+          return {
+            tabBarVisible,
+          };
+        },
+      },
     },
     Insurance: {
-        screen: InsurancePage,
+      screen: InsurancePage,
     },
     MedicalHistory: {
-        screen: MedicalHistoryPage,
+      screen: MedicalHistoryPage,
     },
     History: {
-        screen: HistoryPage,
-        navigationOptions: {
-            headerShown: false,
-        }
+      screen: HistoryPage,
+      navigationOptions: {
+        headerShown: false,
+      },
     },
-}, {
+  },
+  {
     initialRouteName: 'ProfileStack',
     defaultNavigationOptions: {
-    ...TransitionPresets.SlideFromRightIOS,
+      ...TransitionPresets.SlideFromRightIOS,
+      cardStyle: {
+        backgroundColor: '#1F1F1F',
+        opacity: 1,
+      },
     },
-})
+  }
+);
 
 ProfileTab.navigationOptions = ({ navigation }) => {
-    let tabBarVisible = true;
-    if (navigation.state.index > 0) {
-        tabBarVisible = false;
-    }
+  let tabBarVisible = true;
+  if (navigation.state.index > 0) {
+    tabBarVisible = false;
+  }
 
-    return {
-        tabBarVisible,
-    };
+  return {
+    tabBarVisible,
+  };
 };
