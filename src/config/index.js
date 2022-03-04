e45b88f60c1c2b8843102225d8b5bb65cf9f9aa1
Rayendra Sabandar <rayendratimotius@gmail.com>
2022-03-04 16:31:01 +0700

    Added axiosInstance in config.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-let baseURL = 'https://api-medqcare.applimetis.id';
+// let baseURL = 'https://api-medqcare.applimetis.id';
+let baseURL = 'http://192.168.1.5:8080'
+// let webBaseURL = 'https://b9ba-140-213-33-176.ngrok.io'; // URL for postgres drug database
 let webBaseURL = 'https://api-qcareweb.applimetis.id'
 
 const instance = axios.create({
