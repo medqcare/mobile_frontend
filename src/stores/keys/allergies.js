ee70aac09596e85f8d09a21d1babe85908190828
Rayendra Sabandar <rayendratimotius@gmail.com>
2022-03-08 14:56:11 +0700

    Added needInfo key in allergies reducer and keys

--- src/stores/keys/allergies.js
+++ src/stores/keys/allergies.js
@@ -1,10 +1,12 @@
 const SET_ALLERGIES = 'allergies/setAllergies'
+const SET_NEED_INFO = 'allergies/setNeedInfo'
 const SET_ALLERGIES_LOADING = 'allergies/setAllergiesLoading'
 const SET_ALLERGIES_ERROR = 'allergies/setAllergiesError'
 const DELETE_ALLERGIES = 'allergies/deleteAllergies'
 
 const allergyKeys = {
     SET_ALLERGIES,
+    SET_NEED_INFO,
     SET_ALLERGIES_LOADING,
     SET_ALLERGIES_ERROR,
     DELETE_ALLERGIES
