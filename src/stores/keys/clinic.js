93957b6f61dc3dabc35c3ac88b1f89eb95e14f65
Rayendra Sabandar <rayendratimotius@gmail.com>
2022-03-10 12:18:35 +0700

    Created and registered clinic keys and reducer

---
+++ src/stores/keys/clinic.js
@@ -0,0 +1,11 @@
+const SET_CLINIC = 'clinic/setClinic' 
+const SET_CLINIC_LOADING = 'clinic/setClinicLoading' 
+const SET_CLINIC_ERROR = 'clinic/setClinicError'
+
+const clinicKeys = {
+    SET_CLINIC,
+    SET_CLINIC_LOADING,
+    SET_CLINIC_ERROR
+}
+
+export default clinicKeys
