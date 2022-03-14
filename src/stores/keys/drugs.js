174a0969175146b20bd2b72be2395472cf4de955
Rayendra Sabandar <rayendratimotius@gmail.com>
2022-03-14 10:41:58 +0700

    Added set active and finished case in drugReducer

--- src/stores/keys/drugs.js
+++ src/stores/keys/drugs.js
@@ -1,10 +1,14 @@
 const SET_DRUGS = 'drugs/setDrugs'
+const SET_ACTIVE_DRUGS = 'drugs/setActiveDrugs'
+const SET_FINISHED_DRUGS = 'drugs/setFinishedDrugs'
 const SET_DRUGS_LOADING = 'drugs/setDrugsLoading'
 const SET_DRUGS_ERROR = 'drugs/setDrugsError'
 const DELETE_DRUGS = 'drugs/deleteDrugs'
 
 const drugKeys = {
     SET_DRUGS,
+    SET_ACTIVE_DRUGS,
+    SET_FINISHED_DRUGS,
     SET_DRUGS_LOADING,
     SET_DRUGS_ERROR,
     DELETE_DRUGS
