4663dd5f35e656db04d836c5af88c97ce6078899
Rayendra Sabandar <rayendratimotius@gmail.com>
2022-03-17 10:25:22 +0700

    Removed webBaseURL and webInstance from config

--- src/config/index.js
+++ src/config/index.js
@@ -1,21 +1,16 @@
 import axios from 'axios';
 
 let baseURL = 'https://api-medqcare.applimetis.id';
-let webBaseURL = 'https://api-mitralab.medqcare.id'; // URL for postgres drug database
 
 const instance = axios.create({
     baseURL: `${baseURL}/api/v1/members/`,
     headers: {
       'x-secret': 123456 
-    }
-});
-const webInstace = axios.create({
-    baseURL: `${webBaseURL}/api/v1/`,
+    },
+    timeout: 4000
 });
 
 export  {
 	baseURL,
-	webBaseURL,
 	instance,
-	webInstace
 };
