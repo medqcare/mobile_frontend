f9bd115113a81139a5268fd708bbbf75612536f6
Rayendra Sabandar <rayendratimotius@gmail.com>
2022-05-23 16:18:29 +0700

    Created axios function patchNotificationAsViewed

--- src/stores/action/notifications.js
+++ src/stores/action/notifications.js
@@ -6,6 +6,8 @@ const {
     SET_NOTIFICATIONS,
     SET_NOTIFICATIONS_COUNT,
     SET_NOTIFICATIONS_LOADING,
+    SET_NOTIFICATION_ISVIEWED_LOADING,
+    SET_NOTIFICATION_ISDELETED_LOADING,
     SET_NOTIFICATIONS_ERROR,
 } = keys.notificationKeys
 
@@ -52,7 +54,7 @@ export function patchNotificationAsViewed(notificationID, notifications, notific
             console.log(`Application is trying to patch notifications as viewed`)
 
             await dispatch({
-                type: SET_NOTIFICATIONS_LOADING,
+                type: SET_NOTIFICATION_ISDELETED_LOADING,
                 payload: true
             })
 
@@ -77,6 +79,51 @@ export function patchNotificationAsViewed(notificationID, notifications, notific
             await dispatch({
                 type: SET_NOTIFICATIONS_COUNT,
                 payload: newNotificationsCount
+            })
+
+            await dispatch({
+                type: SET_NOTIFICATION_ISDELETED_LOADING,
+                payload: false
+            })
+        } catch (error) {
+            console.log(error.message)
+
+            await dispatch({
+                type: SET_NOTIFICATIONS_ERROR,
+                payload: error.message
+            })
+        }
+    }
+}
+
+export function patchNotificationAsDeleted(notificationID, notifications, notificationsCount){
+    return async dispatch => {
+        try {
+            console.log(`Application is trying to patch notifications as deleted`)
+            
+            await dispatch({
+                type: SET_NOTIFICATION_ISVIEWED_LOADING,
+                payload: true
+            })
+
+            const token = await getToken()
+            const { data } = await instance({
+                method: 'PATCH',
+                url: `notifications/patchIsDeletedValue/${notificationID}`,
+                headers: {
+                    Authorization: token
+                }
+            })
+
+            const newNotificationsList = notifications.filter(el => el._id !== notificationID)
+            await dispatch({
+                type: SET_NOTIFICATIONS,
+                payload: newNotificationsList
+            })
+
+            await dispatch({
+                type: SET_NOTIFICATION_ISVIEWED_LOADING,
+                payload: false
             })
         } catch (error) {
             console.log(error.message)
